5dde0cf66f1a0f18c90fcd863b0eeaa8fdcb6f03
silverymist <26202085+silverymist@users.noreply.github.com>
2023-04-02 12:42:26 +0300

    new import statement!

--- model.py
+++ model.py
@@ -1,4 +1,7 @@
 import torch
+import torch.nn as nn
 
-if __name__ == "__main__":
-    print("Hello world")
+
+if __name__ == '__main__':
+    print('Hello world')
+    print('[current bg, avg bg, last carbs, time since last carbs, last insulin, time since last insulin]')
